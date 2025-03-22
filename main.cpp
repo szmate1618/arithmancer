a1f862fdfeb9cbf218978177d2a5540fbd56cefe
Máté Szabó <szmate1618@gmail.com>
2025-03-22 17:30:36 +0100

    add map.hpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+#define NOMINMAX
 #include "windowing.hpp"
+#include "map.hpp"
 
 
 int main() {
     SetConsoleSize(100, 100);
-    std::cout << "Console resized!" << std::endl;
+
+    Map map(20);
+    map.PrintDungeon();
+
     return 0;
 }
---
+++ map.hpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+const int WIDTH = 100;
+const int HEIGHT = 100;
+
+class Map
+{
+public:
+    Map(size_t roomCount);
+    void PrintDungeon();
+private:
+
+    std::vector<std::vector<char>> grid;
+
+    struct Room {
+        int x, y, w, h;
+    };
+
+    void GenerateDungeon(size_t roomCount);
+};
+
+Map::Map(size_t roomCount): grid(HEIGHT, std::vector<char>(WIDTH))
+{
+    GenerateDungeon(roomCount);
+}
+
+// Print the dungeon
+void Map::PrintDungeon() {
+    for (const auto& row : grid) {
+        for (char cell : row)
+            std::cout << cell;
+        std::cout << '\n';
+    }
+}
+
+// Dungeon generation function
+void Map::GenerateDungeon(size_t roomCount) {
+    std::vector<Room> rooms;
+    srand(time(0));
+
+    // Initialize grid with walls
+    for (int i = 0; i < HEIGHT; ++i)
+        for (int j = 0; j < WIDTH; ++j)
+            grid[i][j] = '#';
+
+    // Place rooms
+    for (int i = 0; i < roomCount; ++i) {
+        int rw = 5 + rand() % 10; // Room width: 5-14
+        int rh = 5 + rand() % 10; // Room height: 5-14
+        int rx = rand() % (WIDTH - rw - 1) + 1;
+        int ry = rand() % (HEIGHT - rh - 1) + 1;
+
+        // Create room
+        Room room = { rx, ry, rw, rh };
+        rooms.push_back(room);
+
+        // Carve out the room
+        for (int y = ry; y < ry + rh; ++y)
+            for (int x = rx; x < rx + rw; ++x)
+                grid[y][x] = '.';
+    }
+
+    // Connect rooms with corridors
+    for (size_t i = 1; i < rooms.size(); ++i) {
+        Room a = rooms[i - 1];
+        Room b = rooms[i];
+
+        int x1 = a.x + a.w / 2;
+        int y1 = a.y + a.h / 2;
+        int x2 = b.x + b.w / 2;
+        int y2 = b.y + b.h / 2;
+
+        // Horizontal tunnel
+        for (int x = std::min(x1, x2); x <= std::max(x1, x2); ++x)
+            grid[y1][x] = '.';
+
+        // Vertical tunnel
+        for (int y = std::min(y1, y2); y <= std::max(y1, y2); ++y)
+            grid[y][x2] = '.';
+    }
+}
