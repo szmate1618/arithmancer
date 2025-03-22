212adfaed15ea99d07f910ea5674a372f1412cf6
Máté Szabó <szmate1618@gmail.com>
2025-03-22 19:22:14 +0100

    add menu.hpp

---
+++ menu.hpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+
+class Menu
+{
+public:
+	Menu();
+	void Draw(std::vector<std::vector<char>>& grid);
+private:
+};
+
+Menu::Menu() {}
+
+void Menu::Draw(std::vector<std::vector<char>>& grid)
+{
+
+}
