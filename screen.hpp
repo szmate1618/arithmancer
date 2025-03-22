bff31096b84b6aadc0a5250ba21ad0f0b4c6d9e6
Máté Szabó <szmate1618@gmail.com>
2025-03-22 19:22:34 +0100

    add screen.hpp

---
+++ screen.hpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+
+class ScreenBuffer {
+private:
+    std::vector<std::vector<char>> buffer;
+    int width;
+    int height;
+
+public:
+    // Constructor
+    ScreenBuffer(int w, int h) : width(w), height(h), buffer(h, std::vector<char>(w, ' ')) {}
+
+    // Getter for width
+    int getWidth() const {
+        return width;
+    }
+
+    // Getter for height
+    int getHeight() const {
+        return height;
+    }
+
+    // Getter for buffer
+    const std::vector<std::vector<char>>& getBuffer() const {
+        return buffer;
+    }
+};
