8b8c294783f008c37b2418ae66682d70766eaa64
Máté Szabó <szmate1618@gmail.com>
2025-03-28 23:19:42 +0100

    add seemingly slightly faster join implementation

--- stringutils.cpp
+++ stringutils.cpp
@@ -4,6 +4,28 @@
 
 
 std::string join(const std::vector<std::string>& strings, const std::string& delimiter) {
-	return std::accumulate(std::next(strings.begin()), strings.end(), strings[0],
-		[&delimiter](const std::string& a, const std::string& b) { return a + delimiter + b; });
+	size_t total_length = 0;
+	for (const std::string& s : strings) {
+		total_length += s.length();
+	}
+	total_length += delimiter.length() * (strings.size() - 1);
+
+	std::string ret(total_length, ' ');
+	size_t index = 0;
+	for (const std::string& s : strings) {
+		for (const char c: s)
+		{
+			ret[index] = c;
+			index++;
+		}
+		if (index < total_length) {
+			for (const char c : delimiter)
+			{
+				ret[index] = c;
+				index++;
+			}
+		}
+	}
+
+	return ret;
 }
